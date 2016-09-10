73b356222c81da57b2be3c32eb9583c318f194a2
eisverticker <eisverticker@gmail.com>
2016-09-10 18:37:17 +0200

    Added Discussion and Comment model and modified info-page model

--- common/models/info-page.js
+++ common/models/info-page.js
@@ -1,19 +1,46 @@
-var toMarkdown = require('marked');
+var marked     = require('marked'),
+    toMarkdown = marked;
+
+var customRenderer = new marked.Renderer();
+
+//alter the image rendering function to render image with responsive bootstrap class
+customRenderer.image = function(href, title, text) {
+  var out = '<img class="img-responsive" src="' + href + '" alt="' + text + '"';
+  if (title) {
+    out += ' title="' + title + '"';
+  }
+  out += this.options.xhtml ? '/>' : '>';
+  return out;
+};
+
+//alter the table rendering function to render table with responsive bootstrap class
+customRenderer.table = function(header, body) {
+  return '<div class="table-responsive"><table>\n'
+    + '<thead>\n'
+    + header
+    + '</thead>\n'
+    + '<tbody>\n'
+    + body
+    + '</tbody>\n'
+    + '</table></div>\n';
+};
 
 module.exports = function(InfoPage) {
 
   InfoPage.validatesUniquenessOf('name');
 
   InfoPage.findByName = function(name, callback){
-    console.log("name",name);
+    if(name === undefined || name === null) return callback("name is undefined");
+
     InfoPage.findOne({
       where: {
         "name": name
       }
     }, function(err, page){
-      if(err) callback(err);
+      if(err) return callback(err);
+      if(page === null) return callback("page not available");
 
-      page.content = toMarkdown(page.content);
+      page.content = toMarkdown(page.content, { renderer: customRenderer });
       callback(null, { "page":page });
     });
   };
@@ -24,7 +51,7 @@ module.exports = function(InfoPage) {
         description: 'Find InfoPage by name and return page with parsed content',
         accessType: 'READ',
         accepts: [
-          {arg: 'name', type: 'string' }
+          {arg: 'name', type: 'string', required: true }
         ],
         http: {path: '/parsed/:name', verb: 'get'},
         returns: { arg: 'page', type: 'object', root: true }
