b3e9733766a0491cac1497cd77e021e2126aac65
eisverticker <eisverticker@gmail.com>
2016-09-11 21:18:54 +0200

    prepared middleware and role-resolver for eventually later usage

---
+++ server/boot/dynamic-roles.js
@@ -0,0 +1,26 @@
+module.exports = function(server) {
+  var Role = server.models.Role;
+  //var creatorModels = ['Comment'];
+
+  /*Role.registerResolver('$creator', function(role, context, cb) {
+    console.log(context.modelName);
+
+
+    // Q: Is the current request accessing a supported model?
+    if (creatorModels.indexOf(context.modelName) === -1) {
+      // A: No. This model is not supported
+      return process.nextTick(() => cb(null, false));
+    }
+
+    //Q: Is the user logged in? (there will be an accessToken with an ID if so)
+    var userId = context.accessToken.userId;
+    if (!userId) {
+      //A: No, user is NOT logged in: callback with FALSE
+      return process.nextTick(() => cb(null, false));
+    }
+
+    //always accept
+    return cb(null, false);
+
+  });*/
+};
---
+++ server/middleware/example.js
@@ -0,0 +1,8 @@
+module.exports = function() {
+  return function example(req, res, next) {
+
+      console.log("example middleware is running");
+      next();
+  };
+
+}
