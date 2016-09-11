3673efcd0793e59b3fbd6816a97064be8e70db09
eisverticker <eisverticker@gmail.com>
2016-09-11 21:16:29 +0200

    Comment and Discussion model are now completed (some hooks and acl)

--- common/models/discussion.js
+++ common/models/discussion.js
@@ -1,3 +1,22 @@
 module.exports = function(Discussion) {
 
+  /**
+   * Adding a comment: Set related user to the userId of the currently logged in user
+   * and set createdAt property
+   */
+  Discussion.beforeRemote("prototype.__create__comments", function( context, comment ,next) {
+
+    //Require user to be authorized
+    var userId = context.req.accessToken.userId;
+    if (!userId) {
+      throw "authorisation required";
+    }
+
+    //Set the related foreign key (userId)
+    context.args.data.appUserId = userId;
+    context.args.data.createdAt = new Date();
+
+    next();
+  });
+
 };
