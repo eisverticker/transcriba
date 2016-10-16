8b3820cd986f8f08b01255135395a335a5f855b4
eisverticker <eisverticker@gmail.com>
2016-10-17 01:56:41 +0200

    created bootscript to automatically create infoPages among others

---
+++ server/boot/dummies.js
@@ -0,0 +1,38 @@
+var faker = require('faker');
+var config = require('../config.json');
+
+module.exports = function(server){
+  if(!config.custom.createDummyData) return;
+  var InfoPage = server.models.InfoPage;
+
+  var createDummyPages = function(pageNames, callback){
+    if(pageNames.length > 0){
+      var pageName = pageNames.pop();
+
+      InfoPage.findOrCreate({
+        where: {
+          "name": pageName
+        }
+      }, {
+        "name": pageName,
+        "content": "Empty page"
+      }, function(err, page){
+        if(err) return callback(err);
+
+        return createDummyPages(pageNames, callback);
+      });
+    }else{
+      callback(null);
+    }
+  };
+
+  //info-page dummies
+  var pageNames = config.custom.dummies.pages;
+  createDummyPages(pageNames, function(err){
+    if(err) throw err;
+
+    console.log("dummy pages created");
+  });
+
+
+}
