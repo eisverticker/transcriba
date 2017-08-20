4d7384c24986de4df1f93e25df23a354953cf6b5
eisverticker <eisverticker@gmail.com>
2017-08-21 00:29:54 +0200

    mongodb-connector: set strictObjectIdCoercion flag as a workaround for #16

---
+++ server/boot/extra-model-configuration.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * This script does some extra configuration on model relations
+ *  and attributes which may not available in the json files
+ */
+module.exports = function(server) {
+  var User = server.models.AppUser;
+  var Role = server.models.Role;
+  var RoleMapping = server.models.RoleMapping;
+
+  //
+  // Problem: mongodb-connectors >= 1.8 are roughly changing the way
+  // how ObjectIds and strings are mapped and thus loading roles
+  // does not work anymore, not even manually implemented in remote method
+  // The following is a workaround and probably causes the 1.7 mechanism
+  // to work again. See #3
+  //
+  RoleMapping.settings.strictObjectIDCoercion = true;
+  //
+
+  //
+  // Connecting AppUser to the Role model
+  // probably not necessary at the moment
+  //
+  // Role.hasMany(User,
+  //   {
+  //     as: 'users',
+  //     through: RoleMapping,
+  //     polymorphic: 'principal',
+  //     invert: true,
+  //   }
+  // );
+};
