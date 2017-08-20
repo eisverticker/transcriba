49367361be135c4798eddd0843ea5cdac6e8d051
eisverticker <eisverticker@gmail.com>
2017-08-21 00:21:01 +0200

    Testing: added admin role test and changed user testing

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -1,14 +1,20 @@
 'use strict';
+/* eslint-disable no-undef, no-unused-vars */
 
 const app = require('../server/server.js');
 const request = require('request');
 const baseUrl = 'http://0.0.0.0:3001';
+const apiUrl = baseUrl + '/api';
 const transcribaConfig = require('../server/transcriba-config.json');
 // const baseUrl = app.get('url').replace(/\/$/, ''); // somehow does not work
 
 // Start server for later api testing
 app.start();
 
+/**
+ * Check whether the server is configurated like expected
+ * TODO: identify more critical configurations and add tests here
+ */
 describe('server', function() {
   it('should have ejs view engine', function() {
     expect(app.get('view engine')).toEqual('ejs');
@@ -22,10 +28,13 @@ describe('server', function() {
   });
 });
 
+/**
+ * Check whether the infoPage module is working
+ */
 describe('infoPage', function() {
   transcribaConfig.dummies.pages.forEach(function(value) {
     it('should load InfoPage ' + value, function(done) {
-      request.get(baseUrl + '/api/InfoPages/parsed/' + value,
+      request.get(apiUrl + '/InfoPages/parsed/' + value,
         function(error, response, body) {
           let bodyObj = JSON.parse(body);
           expect(response.statusCode).toBe(200);
@@ -37,8 +46,18 @@ describe('infoPage', function() {
   });
 });
 
-describe('user', function() {
-  it('should login (admin, see config)', function(done) {
+/**
+ * The following test cases are used to check whether the api works
+ * as expected on actions which require a logged in user
+ * NOTE: in beforeAll we try to get the login data and in the actual test
+ *  cases we build upon that. Indeed this is a problem because the login
+ *  might fail and the login itself should also be a test cases
+ * TODO: provide a better testing solution here (see note)
+ */
+describe('logged in user', function() {
+  var userId, token, loginStatusCode;
+
+  beforeAll(function(done) {
     request.post(
       {
         url: baseUrl + '/api/AppUsers/login',
@@ -51,8 +70,43 @@ describe('user', function() {
       },
       function(error, response, body) {
         // NOTE: body is already an parsed json object
+        if (error) {
+          done.fail('login request: error occured during initialisation');
+        } else {
+          loginStatusCode = response.statusCode;
+          if (
+            body === undefined ||
+            body.userId === undefined ||
+            body.id === undefined
+          ) {
+            done.fail('login request: admin login has failed');
+          } else {
+            userId = body.userId;
+            token = body.id;
+            done();
+          }
+        }
+      }
+    );
+  });
+
+  //
+  // NOTE: this was disabled due to testing architectue (see todo above)
+  //  this might be useful later, but if not just remove it
+  //
+  // it('should login the user', function(done) {
+  //   expect(loginStatusCode).toBe(300);
+  //   expect(userId !== undefined).toBe(true);
+  // });
+
+  it('should have admin role', function(done) {
+    request.get(apiUrl + '/AppUsers/' + userId + '/roles?access_token=' + token,
+      function(error, response, body) {
+        let roles = JSON.parse(body).map(
+          (roleEntity) => roleEntity.name
+        );
         expect(response.statusCode).toBe(200);
-        expect(body.userId !== undefined).toBe(true);
+        expect(roles).toContain('administrator');
         done();
       }
     );
