e941e1ebf2a5a6852cfa2f63c068323e5e29ef41
eisverticker <eisverticker@posteo.de>
2018-01-07 22:18:44 +0100

    Exception-Handling: added central file where exception types are being stored

---
+++ common/exceptions.js
@@ -0,0 +1,28 @@
+'use strict';
+/**
+ * Central Collection of Errors / Exceptions which are being used
+ * throughout the application
+ */
+// TODO
+
+module.exports = {
+  Unclear: new Error('Nonspecified Exception'),
+  NotFound: {
+    Default: new Error('model(s) not found'),
+    TranscribaObject: new Error('trObject was not found'),
+    Collection: new Error('Collection was not found'),
+    User: new Error('User was not found'),
+    Role: new Error('Role was not found'),
+    Directory: new Error('directory does not exist'),
+    Revision: new Error('revision(s) not found'),
+    RelatedModel: new Error('cannot find related model')
+  },
+  WrongFormat: new Error('Expected data format was different'),
+  Duplicate: new Error('Cannot create a duplicate model'),
+  Replay: new Error('Action was already done'),
+  Occupied: new Error('Cannot access model because it is already occupied'),
+  BusyUser: new Error('User is busy'),
+  WrongInput: new Error('Lack of mandatory input parameters'),
+  Unauthorized: new Error('User has not the required permissions'),
+  Forbidden: new Error('This action is not allowed')
+};
