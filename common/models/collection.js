c973670d69e8a790d7dfa5a6a817c1f9a50aa6ed
eisverticker <eisverticker@posteo.de>
2018-01-11 03:12:25 +0100

    Tech-Change: Added prototype-related method definitions to model definition files and rewrote a lot of them

--- common/models/collection.js
+++ common/models/collection.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = function(Collection) {
+  /**
+   * Current completion status of this collection
+   * @param {Function(Error, number)} callback
+   */
 
+  Collection.prototype.progress = function() {
+    let progressPercentage = 5;
+    // TODO
+    return Promise.resolve(progressPercentage);
+  };
 };
